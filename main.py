cb276624070fa0416e4e5d3f5d5f4411233627d6
atrujillo-1 <atrujillo@itba.edu.ar>
2026-03-10 17:04:11 -0300

    nuevo intento

--- main.py
+++ main.py
@@ -14,6 +14,35 @@ def dado():
 def hola_mundo():
     return "hola_mundo"
 
+def turno1():
+    finaliza = False
+    dados_guardados = []
+    dados_tirada = []
+    nro_tirada = 0
+    dados_a_tirar = 5
+    while finaliza == False and nro_tirada < 3:
+        
+        nro_dado = 0
+        for nro_dado in range(dados_a_tirar):  
+            valor = dado()
+            dados_tirada.append(valor)
+            nro_dado +=1
+        print(f'Tirada {dados_tirada}')
+        finalizar= upper_custom(input("Desea finalizar su turno?: "))
+        if finalizar=="SI":
+            finaliza=True
+            print(dados_tirada)
+        else:
+            for k in range(len(dados_tirada)):
+                print(dados_tirada[k])
+                guardar= upper_custom(input("Desea guardar este dado?: "))
+                if guardar == "SI":
+                    dados_guardados.append(dados_tirada[k])
+            print(f'Dados guardados: {dados_guardados}')
+            dados_tirada = dados_guardados
+            dados_a_tirar = 5-len(dados_guardados)
+            nro_tirada +=1
+            
 
 def turno():
     finaliza=False
@@ -45,3 +74,5 @@ def turno():
                 finaliza=False
             i+=1
     print(dados_guardados)
+    
+turno1()
